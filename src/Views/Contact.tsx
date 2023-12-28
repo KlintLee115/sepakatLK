5c733ceb1fae560b914e84733f494605c4b7f42d
KlintLee115 <54990359+KlintLee115@users.noreply.github.com>
2023-12-28 15:18:14 -0700

    updated contacts for dark theme

--- src/Views/Contact.tsx
+++ src/Views/Contact.tsx
@@ -11,7 +11,6 @@ export default function Contact() {
         padding: '4% 2%',
         textAlign: 'center',
         margin: '5% 0',
-        border: '1px solid #303745',
         borderRadius: '3%',
         flexGrow: 0,
         flexShrink: 0,
@@ -27,20 +26,20 @@ export default function Contact() {
         <NavBar currView="CONTACT US" />
 
         <main className='flex justify-around p-[4%]' style={{ "fontFamily": 'Lato' }}>
-            <div style={ContactWrapperStyle} className='dark:border-white'>
+            <div style={ContactWrapperStyle} className='border border-gray-800 dark:border-white'>
                 <div className='mx-auto w-min'><HiLocationMarker /></div>
                 <p>1-1, Jalan PUJ 3/5,
                     Taman Puncak Jalil
                     Bandar Putra Permai
                     43300 Selangor.</p>
             </div>
-            <div style={ContactWrapperStyle} className='dark:border-white'>
+            <div style={ContactWrapperStyle} className='border border-gray-800 dark:border-white'>
                 <div className='mx-auto w-min'><AiFillPhone /></div>
                 <p>Telephone: +603-8940 1833</p>
                 <p>Fax: +603-8940 2833</p>
 
             </div>
-            <div style={ContactWrapperStyle} className='dark:border-white'>
+            <div style={ContactWrapperStyle} className='border border-gray-800 dark:border-white'>
                 <div className='mx-auto w-min'> <HiMailOpen /></div>
                 <p>Email: sepakatlk@yahoo.com</p>
             </div>
